844df79112a2ef66159707382bfe6f86cab1e5e4
André Moura <andre.benedicto@etec.sp.gov.br>
2020-08-14 20:22:15 -0300

    Aplicando scroll dinâmico nos menus

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 $(() => {
 
+    // Menu responsivo
     $('#menu').click(e => {
         $(e.target).find('ul').slideToggle()
+    })
+
+    // Scroll dinâmico nos menus
+    $('nav a').click(e => {
+        let href = $(e.target).attr('href')
+        let offSetTop = $(href).offset().top
+
+        $('html, body').animate({
+            'scrollTop': offSetTop
+        })
     })
 
 })

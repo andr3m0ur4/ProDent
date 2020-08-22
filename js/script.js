477c17dfdebb1cef142f4e575a98a7f5053269dd
André Moura <andre.benedicto@etec.sp.gov.br>
2020-08-21 21:42:03 -0300

    Aplicando slider através do plugin Slick

--- js/script.js
+++ js/script.js
@@ -15,4 +15,67 @@ $(() => {
         })
     })
 
+    // Slick
+    $('.mosaico .container .mosaico-wrapper').slick({
+        centerMode: false,
+        slidesToShow: 6,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                    infinite: false,
+                    initialSlide: 1
+                }
+            },
+            {
+                breakpoint: 580,
+                settings: {
+                    slidesToShow: 2,
+                    infinite: false,
+                    initialSlide: 2
+                }
+            },
+            {
+                breakpoint: 380,
+                settings: {
+                    slidesToShow: 1,
+                    infinite: false,
+                    initialSlide: 3
+                }
+            }
+        ]
+    })
+
+    $('.tratamentos .container').slick({
+        centerMode: false,
+        slidesToShow: 3,
+        arrows: false,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    dots: true,
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    dots: true,
+                    slidesToShow: 1
+                }
+            }
+        ]
+    })
+
+    $('.depoimentos .container').slick({
+        centerMode: false,
+        arrows: false,
+        infinite: false,
+        dots: true
+    })
+
 })
